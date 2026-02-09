e3380b35fe870207cf3a63f4aa564ce721ea5563
Sonia <143387635+Soniadevpro@users.noreply.github.com>
2026-02-09 14:56:35 +0100

    Add start script for Railway

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install -r backend/requirements.txt
+
+python backend/manage.py collectstatic --no-input
+python backend/manage.py migrate
+
+gunicorn backend.config.wsgi:application --bind 0.0.0.0:$PORT
